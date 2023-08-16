c1d6fec14ec4780b918f8a4dd9fbbcc88c383cf8
gog1 <gogstudio.room@gmail.com>
2023-08-16 19:25:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+#翻訳するテキストを聞く
 text = input("Enter:")
+#エンコードして"encoded"に代入する(バイト列)
 encoded = text.encode("ISO-2022-JP")
+#"encodedを出力する"
 print(encoded)
+
+#バイト列を整数にしてリスト"hex_representation"に代入
 hex_representation = [f'{byte:02X}' for byte in encoded]
 del hex_representation[:3]
 del hex_representation[-3:]
