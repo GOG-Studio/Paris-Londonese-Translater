0a5dc199aedb85e4bf899a5d72f002680bf47042
gog1 <gogstudio.room@gmail.com>
2023-08-16 22:40:46 +0900

    処理を関数化

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
-#翻訳するテキストを聞く
-text = input("Enter:")
-#エンコードして"encoded"に代入する(バイト列)
-encoded = text.encode("ISO-2022-JP")
-#"encodedを出力する"
-#print(encoded)
 
-#バイト列を整数にしてリスト"hex_representation"に代入
-hex_representation = [f'{byte:02X}' for byte in encoded]
-#リスト"hex_representation"の前後三つを削除して代入
-del hex_representation[:3]
-del hex_representation[-3:]
-#リスト"hex_representation"から偶数・奇数 番目を取り出してそれぞれリスト"first_hex""second_hexに代入する"
-first_hex = hex_representation[1::2]
-second_hex = hex_representation[::2]
+def translate_letter(text):
+    #エンコードして"encoded"に代入する(バイト列)
+    encoded = text.encode("ISO-2022-JP")
+    #"encodedを出力する"
+    #print(encoded)
 
-#それぞれのリストの文字列を数列に変換する
-first_hex = [int(x,16) for x in first_hex]
-second_hex = [int(x,16) for x in second_hex]
-#条件ごとにリスト内の数列から減算する
-first_hex_a = [x - 32 if 0x21 <= x <= 0x28 else x - 39 if 0x30 <= x <= 0x90 else x for x in first_hex]
-second_hex_a = [x - 32 for x in second_hex]
+    #バイト列を整数にしてリスト"hex_representation"に代入
+    hex_representation = [f'{byte:02X}' for byte in encoded]
+    #リスト"hex_representation"の前後三つを削除して代入
+    del hex_representation[:3]
+    del hex_representation[-3:]
+    #リスト"hex_representation"から偶数・奇数 番目を取り出してそれぞれリスト"first_hex""second_hexに代入する"
+    first_hex = hex_representation[1::2]
+    second_hex = hex_representation[::2]
 
-#減算後の値を出力する（3,4行目）
-#print(second_hex_a)
-#print(first_hex_a)
+    #それぞれのリストの文字列を数列に変換する
+    first_hex = [int(x,16) for x in first_hex]
+    second_hex = [int(x,16) for x in second_hex]
+    #条件ごとにリスト内の数列から減算する
+    first_hex_a = [x - 32 if 0x21 <= x <= 0x28 else x - 39 if 0x30 <= x <= 0x90 else x for x in first_hex]
+    second_hex_a = [x - 32 for x in second_hex]
 
-tr = str.maketrans({"1":"ぱ","2":"り","3":"ろ","4":"ん","5":"ど","6":"パ","7":"リ","8":"ロ","9":"ン","0":"ド",})
-word = str(str(second_hex_a[0])+"ー"+str(first_hex_a[0])).translate(tr)
+    #減算後の値を出力する（3,4行目）
+    #print(second_hex_a)
+    #print(first_hex_a)
 
-print(word)
+    tr = str.maketrans({"1":"ぱ","2":"り","3":"ろ","4":"ん","5":"ど","6":"パ","7":"リ","8":"ロ","9":"ン","0":"ド",})
+    word = str(str(second_hex_a[0])+"ー"+str(first_hex_a[0])).translate(tr)
+    return word
+
+word = input("翻訳する文字（全角）を入力：")
+translated = translate_letter(word)
+print(translated)
